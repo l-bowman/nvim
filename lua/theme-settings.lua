c30bb462b9c79e9f87999fe6afec021b6706a969
Luke Bowman <luke.bowman@wyyerd.com>
2022-09-14 11:42:40 -0600

    give me night back

--- lua/theme-settings.lua
+++ lua/theme-settings.lua
@@ -3,7 +3,7 @@ vim.g.tokyonight_transparent = true
 vim.g.tokyonight_transparent_sidebar = true
 vim.g.tokyonight_dark_float = false
 -- vim.g.tokyonight_hide_inactive_statusline = true
-vim.cmd([[colorscheme tokyonight]])
+vim.cmd([[colorscheme tokyonight-night]])
 
 local colors = require("tokyonight.colors").setup({})
 
