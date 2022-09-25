cd5844b8bd6d2d634730f0899b3996b5be8b0661
Luke Bowman <luke.bowman@wyyerd.com>
2022-09-25 18:34:27 -0500

    merged upstream

--- lua/load-plugins.lua
+++ lua/load-plugins.lua
@@ -154,7 +154,7 @@ return require("packer").startup(function(use)
     "rmagatti/auto-session",
     config = function()
       require("auto-session").setup({
-        log_level = "info",
+        log_level = "error",
         auto_session_suppress_dirs = { "~/", "~/Projects" },
       })
     end,
