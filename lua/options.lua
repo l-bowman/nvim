280f7ab4ed6d6c9e1f2f98e159a8d131ce660dee
grognaak <luke.s.bowman@gmail.com>
2022-10-10 01:34:54 -0400

    removed obsolete stuff

--- lua/options.lua
+++ lua/options.lua
@@ -58,14 +58,3 @@ vim.g.markdown_fenced_languages = {
   "bash",
   "ts=typescript",
 }
---TODO; Convert this to lua.
-vim.cmd([[
-command! -nargs=0 -bar Qargs execute 'args' QuickfixFilenames()
-function! QuickfixFilenames()
-  let buffer_numbers = {}
-  for quickfix_item in getqflist()
-    let buffer_numbers[quickfix_item['bufnr'] ] = bufname(quickfix_item['bufnr'])
-  endfor
-  return join(map(values(buffer_numbers), 'fnameescape(v:val)'))
-endfunction
-]])
