821eb283412a082f2b2ebcdbd15b18dc4f386d65
Luke Bowman <luke.bowman@wyyerd.com>
2023-02-28 10:21:10 -0600

    vue server

--- lua/servers/vue.lua
+++ lua/servers/vue.lua
@@ -1,13 +1,12 @@
 -- Vue JS --------------------------------------
-require("lspconfig").volar.setup({})
+-- require("lspconfig").volar.setup({})
 
 -- Vue JS --------------------------------------
--- -- require("lspconfig").vuels.setup({})
--- require("lspconfig").volar.setup({
---   init_options = {
---       typescript = {
---             tsdk = os.getenv("HOME") ..
---             "/.nvm/versions/node/v14.19.0/lib/node_modules/typescript/lib",
---                 },
---                   },
---                   })
+-- require("lspconfig").vuels.setup({})
+require("lspconfig").volar.setup({
+  init_options = {
+    typescript = {
+      tsdk = os.getenv("HOME") .. "/.nvm/versions/node/v14.19.0/lib/node_modules/typescript/lib",
+    },
+  },
+})
