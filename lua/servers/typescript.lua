ad24d0ecfceb806ce17ec74f24d70ab285a3ee7c
Luke Bowman <luke.bowman@wyyerd.com>
2023-03-15 15:12:49 -0600

    Volar takeover mode

--- lua/servers/typescript.lua
+++ lua/servers/typescript.lua
@@ -1,15 +1,16 @@
 -- JavaScript and TypeScript -------------------------------
-local root_pattern = require("lspconfig").util.root_pattern
-
-require("lspconfig").tsserver.setup({
-  filetypes = {
-    "javascript",
-    "javascriptreact",
-    "javascript.jsx",
-    "typescript",
-    "typescriptreact",
-    "typescript.tsx",
-  },
-  cmd = { "typescript-language-server", "--stdio" },
-  root_dir = root_pattern("package.json", "tsconfig.json", "jsconfig.json", "index.js", "app.js"),
-})
+-- DISABLED to enable Volar Take Over Mode
+-- local root_pattern = require("lspconfig").util.root_pattern
+--
+-- require("lspconfig").tsserver.setup({
+--   filetypes = {
+--     "javascript",
+--     "javascriptreact",
+--     "javascript.jsx",
+--     "typescript",
+--     "typescriptreact",
+--     "typescript.tsx",
+--   },
+--   cmd = { "typescript-language-server", "--stdio" },
+--   root_dir = root_pattern("package.json", "tsconfig.json", "jsconfig.json", "index.js", "app.js"),
+-- })
--- lua/servers/vue.lua
+++ lua/servers/vue.lua
@@ -4,6 +4,8 @@
 -- Vue JS --------------------------------------
 -- require("lspconfig").vuels.setup({})
 require("lspconfig").volar.setup({
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+
   init_options = {
     typescript = {
       tsdk = os.getenv("HOME") .. "/.nvm/versions/node/v14.19.0/lib/node_modules/typescript/lib",
