6df6ed2fa0205058fa56ddd57963ca3aa82f708a
Luke Bowman <luke.bowman@wyyerd.com>
2023-03-23 15:14:47 -0600

    wip

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,4 +1,24 @@
+-- local function is_inside_specific_folder()
+--   local target_folder = "/Users/lukebowman/Documents/dev/codereview"
+--   local current_working_directory = vim.fn.getcwd()
+--   return current_working_directory:find(target_folder, 1, true) ~= nil
+-- end
+local function is_on_master_branch()
+  local git_branch_cmd = "git rev-parse --abbrev-ref HEAD 2>/dev/null"
+  local current_branch = vim.fn.system(git_branch_cmd):gsub("%s+", "")
+
+  return current_branch == "master"
+end
+
+local function custom_warning_indicator()
+  if is_on_master_branch() then
+    return "CAUTION! " .. string.rep("⚠️", 10) .. " WORKING ON MASTER BRANCH!" -- This will create a string with 10 warning symbols
+  else
+    return ""
+  end
+end
 return {
+
   {
     "nvim-lualine/lualine.nvim",
     opts = {
@@ -13,11 +33,13 @@ return {
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch" },
-        lualine_c = { { "filename", file_status = true, path = 1 } },
-        -- lualine_x = {"encoding", "fileformat", "filetype"},
-        lualine_x = { "encoding", "filetype" },
+        lualine_c = { "encoding", "filetype", custom_warning_indicator },
         lualine_y = { "progress" },
         lualine_z = { "location" },
+        lualine_x = {
+          { "filename", file_status = true, path = 1 },
+        },
+        -- lualine_x = {"encoding", "fileformat", "filetype"},
       },
       inactive_sections = {
         lualine_a = {},
