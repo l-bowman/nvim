4ed9f10dd4b5ff3921c2a72b88cbb4aee46570d3
Luke Bowman <luke.bowman@wyyerd.com>
2023-03-23 13:40:55 -0600

    wip

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -47,7 +47,7 @@ return {
         },
         c = { "<cmd>CodeActionMenu<CR>", "Code Actions" },
         d = { "<cmd>lua vim.diagnostic.open_float({ border = 'rounded' })<CR>", "Line Diagnostics" },
-        D = { "<cmd>delmarks *<CR>", "Delete All Marks" },
+        D = { "<cmd>delm! | delm A-Z0-9<CR>", "Delete All Marks" },
         e = { "<cmd>NvimTreeToggle<CR>", "File Tree" },
         f = {
           name = "Find with Telescope",
