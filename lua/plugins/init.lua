451318c918f45ca66217047b8709cd6897ebb750
Luke Bowman <luke.bowman@wyyerd.com>
2023-05-01 12:36:42 -0600

    wip

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,15 +23,15 @@ return {
   { "j-hui/fidget.nvim", opts = { window = { border = "rounded", blend = 0 } } },
 
   { "numToStr/Comment.nvim", opts = {} },
-  -- { "rmagatti/auto-session", opts = { auto_session_use_git_branch = true } },
-  {
-    "rmagatti/auto-session",
-    config = function()
-      require("auto-session").setup({
-        opts = { auto_session_use_git_branch = true },
-      })
-    end,
-  },
+  { "rmagatti/auto-session", opts = { auto_session_use_git_branch = true } },
+  -- {
+  --   "rmagatti/auto-session",
+  --   config = function()
+  --     require("auto-session").setup({
+  --       opts = { auto_session_use_git_branch = true },
+  --     })
+  --   end,
+  -- },
   -- "airblade/vim-gitgutter",
   "mhinz/vim-signify",
   "weilbith/nvim-code-action-menu",
