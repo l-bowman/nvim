7308e2da40070dce098c42b6b7ca83747ce4866e
Luke Bowman <luke.bowman@wyyerd.com>
2023-05-18 11:54:06 -0600

    wip

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -3,7 +3,9 @@ return {
     "rmagatti/session-lens",
     dependencies = { "rmagatti/auto-session", "nvim-telescope/telescope.nvim" },
     config = function()
-      require("auto-session").setup({})
+      require("auto-session").setup({
+        opts = { auto_session_use_git_branch = true },
+      })
     end,
   },
 }
