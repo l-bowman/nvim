1f8859030a1d0b46cd35faf0d93b28662dd12d6e
Luke Bowman <luke.bowman@wyyerd.com>
2023-08-08 14:15:06 -0600

    wip

--- lua/servers/vue.lua
+++ lua/servers/vue.lua
@@ -9,9 +9,10 @@ require("lspconfig").volar.setup({
   init_options = {
     typescript = {
       -- tsdk = os.getenv("HOME") .. "/.nvm/versions/node/v14.19.0/lib/node_modules/typescript/lib",
-      -- tsdk = os.getenv("HOME") .. "/.nvm/versions/node/v16.19.0/lib/node_modules/typescript/lib",
+      tsdk = os.getenv("HOME") .. "/.nvm/versions/node/v16.19.0/lib/node_modules/typescript/lib",
       -- tsdk = "./node_modules/typescript/lib",
-      tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+      -- tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+      -- tsdk = "/Users/lukebowman/Documents/dev/monorepo/node_modules/typescript/lib",
     },
   },
 })
