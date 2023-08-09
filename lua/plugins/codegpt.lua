653eed0eedb7515adedef2d97a557bd27f43fc35
Luke Bowman <luke.bowman@wyyerd.com>
2023-08-09 11:19:12 -0600

    wip

--- lua/plugins/codegpt.lua
+++ lua/plugins/codegpt.lua
@@ -7,10 +7,28 @@ return {
     },
     config = function()
       require("codegpt.config")
-      vim.g["codegpt_commands_defaults"] = {
-        ["completion"] = {
-          model = "gpt-4",
-          temperture = "0.9",
+      vim.g["codegpt_commands"] = {
+        ["code_edit"] = {
+          model = "gpt-3.5-turbo",
+          -- model = "gpt-4",
+          max_tokens = 4096,
+          temperature = 0.6,
+          -- temperature = 0.9,
+          number_of_choices = 1,
+          system_message_template = "",
+          user_message_template = "",
+          callback_type = "replace_lines",
+        },
+        ["chat"] = {
+          model = "gpt-3.5-turbo",
+          -- model = "gpt-4",
+          max_tokens = 4096,
+          temperature = 0.6,
+          -- temperature = 0.9,
+          number_of_choices = 1,
+          system_message_template = "",
+          user_message_template = "",
+          callback_type = "replace_lines",
         },
       }
     end,
